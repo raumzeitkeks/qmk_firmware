089c446752225ad575fea7233afca0c825a2bdef
Calvin Ruocco <calvin@ruoc.co>
2024-09-06 11:36:17 +0200

    v4

--- keyboards/splitkb/kyria/keymaps/raumzeitkeks/keymap.c
+++ keyboards/splitkb/kyria/keymaps/raumzeitkeks/keymap.c
@@ -32,54 +32,54 @@ enum custom_keycodes {
 
     // Must be in the same order as in the symbols array!
     _SYMBOLS_START,
-    SYM_TILD,
-    SYM_COMM,
-    SYM_EXLM,
-    SYM_DOT,
-    SYM_DLR,
-    SYM_QUOT,
-    SYM_EQL,
-    SYM_CIRC,
-    SYM_LPRN,
-    SYM_RPRN,
-    SYM_LCBR,
-    SYM_RCBR,
-    SYM_EURO,
-    SYM_SLSH,
-    SYM_ASTR,
-    SYM_PLUS,
-    SYM_PERC,
-    SYM_MINS,
-    NUM_TILD,
-    NUM_COMM,
-    NUM_DOT,
-    NUM_EQL,
-    NUM_CIRC,
-    NUM_LPRN,
-    NUM_RPRN,
-    NUM_LABK,
-    NUM_RABK,
-    NUM_EURO,
-    NUM_SLSH,
-    NUM_ASTR,
-    NUM_PLUS,
-    NUM_PERC,
-    NUM_MINS,
+    CS_TILD,
+    CS_COMM,
+    CS_EXLM,
+    CS_DOT,
+    CS_DLR,
+    CS_QUOT,
+    CS_EQL,
+    CS_CIRC,
+    CS_LPRN,
+    CS_RPRN,
+    CS_LCBR,
+    CS_RCBR,
+    CS_EURO,
+    CS_SLSH,
+    CS_ASTR,
+    CS_PLUS,
+    CS_PERC,
+    CS_MINS,
+    CM_TILD,
+    CM_COMM,
+    CM_DOT,
+    CM_EQL,
+    CM_CIRC,
+    CM_LPRN,
+    CM_RPRN,
+    CM_LABK,
+    CM_RABK,
+    CM_EURO,
+    CM_SLSH,
+    CM_ASTR,
+    CM_PLUS,
+    CM_PERC,
+    CM_MINS,
     _SYMBOLS_STOP,
     _SYMBOLS_NUM = _SYMBOLS_STOP - _SYMBOLS_START - 1,
 
     _NAVIGATION_START,
-    NAV_COPY,
-    NAV_PSTE,
-    NAV_UNDO,
-    NAV_MUTE,
+    CN_COPY,
+    CN_PSTE,
+    CN_UNDO,
+    CN_MUTE,
     _NAVIGATION_STOP,
     _NAVIGATION_NUM = _NAVIGATION_STOP - _NAVIGATION_START - 1,
 
     _TAPPING_START,
-    TAP_INCR,
-    TAP_DECR,
-    TAP_SHOW,
+    CT_INCR,
+    CT_DECR,
+    CT_SHOW,
     _TAPPING_STOP,
     _TAPPING_NUM = _TAPPING_STOP - _TAPPING_START - 1,
 };
@@ -94,39 +94,39 @@ typedef struct {
 // Must be in the same order as in the custom_keycodes enum!
 const symbol_t symbols[_SYMBOLS_NUM + 1] = {
     { KC_NO,   KC_NO,   false, false },  // not a symbol
-    { DE_TILD, KC_NO,   false, false },  // SYM_TILD_NO
-    { DE_COMM, DE_SCLN, false, false },  // SYM_COMM_SCLN
-    { DE_EXLM, DE_QUES, false, false },  // SYM_EXLM_QUES
-    { DE_DOT,  DE_COLN, false, false },  // SYM_DOT_COLN
-    { DE_DLR,  DE_HASH, false, false },  // SYM_DLR_HASH
-    { DE_QUOT, DE_DQUO, false, false },  // SYM_QUOT_DQUO
-    { DE_EQL,  DE_GRV,  false, true  },  // SYM_EQL_GRV
-    { DE_CIRC, DE_DEG,  true,  false },  // SYM_CIRC_DEG
-    { DE_LPRN, DE_LBRC, false, false },  // SYM_LPRN_LBRC
-    { DE_RPRN, DE_RBRC, false, false },  // SYM_RPRN_RBRC
-    { DE_LCBR, DE_LABK, false, false },  // SYM_LCBR_LABK
-    { DE_RCBR, DE_RABK, false, false },  // SYM_RCBR_RABK
-    { DE_EURO, DE_AT,   false, false },  // SYM_EURO_AT
-    { DE_SLSH, DE_BSLS, false, false },  // SYM_SLSH_BSLS
-    { DE_ASTR, DE_PIPE, false, false },  // SYM_ASTR_PIPE
-    { DE_PLUS, DE_AMPR, false, false },  // SYM_PLUS_AMPR
-    { DE_PERC, KC_NO,   false, false },  // SYM_PERC_NO
-    { DE_MINS, DE_UNDS, false, false },  // SYM_MINS_UNDS
-    { DE_TILD, DE_TILD, false, false },  // NUM_TILD
-    { DE_COMM, DE_COMM, false, false },  // NUM_COMM
-    { DE_DOT,  DE_DOT,  false, false },  // NUM_DOT
-    { DE_EQL,  DE_EQL,  false, false },  // NUM_EQL
-    { DE_CIRC, DE_CIRC, true,  true  },  // NUM_CIRC
-    { DE_LPRN, DE_LPRN, false, false },  // NUM_LPRN
-    { DE_RPRN, DE_RPRN, false, false },  // NUM_RPRN
-    { DE_LABK, DE_LABK, false, false },  // NUM_LABK
-    { DE_RABK, DE_RABK, false, false },  // NUM_RABK
-    { DE_EURO, DE_EURO, false, false },  // NUM_EURO
-    { DE_SLSH, DE_SLSH, false, false },  // NUM_SLSH
-    { DE_ASTR, DE_ASTR, false, false },  // NUM_ASTR
-    { DE_PLUS, DE_PLUS, false, false },  // NUM_PLUS
-    { DE_PERC, DE_PERC, false, false },  // NUM_PERC
-    { DE_MINS, DE_MINS, false, false },  // NUM_MINS
+    { DE_TILD, KC_NO,   false, false },  // CS_TILD_NO
+    { DE_COMM, DE_SCLN, false, false },  // CS_COMM_SCLN
+    { DE_EXLM, DE_QUES, false, false },  // CS_EXLM_QUES
+    { DE_DOT,  DE_COLN, false, false },  // CS_DOT_COLN
+    { DE_DLR,  DE_HASH, false, false },  // CS_DLR_HASH
+    { DE_QUOT, DE_DQUO, false, false },  // CS_QUOT_DQUO
+    { DE_EQL,  DE_GRV,  false, true  },  // CS_EQL_GRV
+    { DE_CIRC, DE_DEG,  true,  false },  // CS_CIRC_DEG
+    { DE_LPRN, DE_LBRC, false, false },  // CS_LPRN_LBRC
+    { DE_RPRN, DE_RBRC, false, false },  // CS_RPRN_RBRC
+    { DE_LCBR, DE_LABK, false, false },  // CS_LCBR_LABK
+    { DE_RCBR, DE_RABK, false, false },  // CS_RCBR_RABK
+    { DE_EURO, DE_AT,   false, false },  // CS_EURO_AT
+    { DE_SLSH, DE_BSLS, false, false },  // CS_SLSH_BSLS
+    { DE_ASTR, DE_PIPE, false, false },  // CS_ASTR_PIPE
+    { DE_PLUS, DE_AMPR, false, false },  // CS_PLUS_AMPR
+    { DE_PERC, KC_NO,   false, false },  // CS_PERC_NO
+    { DE_MINS, DE_UNDS, false, false },  // CS_MINS_UNDS
+    { DE_TILD, DE_TILD, false, false },  // CM_TILD
+    { DE_COMM, DE_COMM, false, false },  // CM_COMM
+    { DE_DOT,  DE_DOT,  false, false },  // CM_DOT
+    { DE_EQL,  DE_EQL,  false, false },  // CM_EQL
+    { DE_CIRC, DE_CIRC, true,  true  },  // CM_CIRC
+    { DE_LPRN, DE_LPRN, false, false },  // CM_LPRN
+    { DE_RPRN, DE_RPRN, false, false },  // CM_RPRN
+    { DE_LABK, DE_LABK, false, false },  // CM_LABK
+    { DE_RABK, DE_RABK, false, false },  // CM_RABK
+    { DE_EURO, DE_EURO, false, false },  // CM_EURO
+    { DE_SLSH, DE_SLSH, false, false },  // CM_SLSH
+    { DE_ASTR, DE_ASTR, false, false },  // CM_ASTR
+    { DE_PLUS, DE_PLUS, false, false },  // CM_PLUS
+    { DE_PERC, DE_PERC, false, false },  // CM_PERC
+    { DE_MINS, DE_MINS, false, false },  // CM_MINS
 };
 
 size_t get_symbol_index(uint16_t keycode) {
@@ -213,10 +213,10 @@ bool process_navigation(uint16_t keycode, keyrecord_t *record) {
     clear_mods();
 
     switch (keycode) {
-        case NAV_COPY: tap_code16(is_shift_pressed ? C(DE_X) : C(DE_C)); break;
-        case NAV_PSTE: tap_code16(C(DE_V)); break;
-        case NAV_UNDO: tap_code16(is_shift_pressed ? C(DE_Y) : C(DE_Z)); break;
-        case NAV_MUTE: tap_code16(KC_MUTE); break;
+        case CN_COPY: tap_code16(is_shift_pressed ? C(DE_X) : C(DE_C)); break;
+        case CN_PSTE: tap_code16(C(DE_V)); break;
+        case CN_UNDO: tap_code16(is_shift_pressed ? C(DE_Y) : C(DE_Z)); break;
+        case CN_MUTE: tap_code16(KC_MUTE); break;
     }
 
     // Reset modifiers to original state
@@ -258,9 +258,9 @@ bool process_tapping_term(uint16_t keycode, keyrecord_t *record) {
     }
 
     switch (keycode) {
-        case TAP_INCR: g_tapping_term += 10; break;
-        case TAP_DECR: g_tapping_term -= 10; break;
-        case TAP_SHOW: {
+        case CT_INCR: g_tapping_term += 10; break;
+        case CT_DECR: g_tapping_term -= 10; break;
+        case CT_SHOW: {
             const char* str = get_u16_str(g_tapping_term, ' ');
             for (; *str != '\0'; ++str) {
                 if ('0' <= *str && *str <= '9') {
@@ -316,57 +316,57 @@ bool process_record_user(uint16_t keycode, keyrecord_t* record) {
 #define _X_ KC_NO
 #define __XXX__ KC_NO
 
-#define TUP TAP_INCR
-#define TDN TAP_DECR
-#define TSH TAP_SHOW
+#define CTI CT_INCR
+#define CTD CT_DECR
+#define CTS CT_SHOWs
 
 #define DENSE_LAYOUT(L31, L32, L33, L34, L35, R35, R34, R33, R32, R31, L20, L21, L22, L23, L24, L25, R25, R24, R23, R22, R21, R20, L11, L12, L13, L14, L15, R15, R14, R13, R12, R11, L01, L02, L03, L04, R04, R03, R02, R01) \
     LAYOUT( _X_, L31, L32, L33, L34, L35,                     R35, R34, R33, R32, R31, _X_, \
             L20, L21, L22, L23, L24, L25,                     R25, R24, R23, R22, R21, R20, \
-            _X_, L11, L12, L13, L14, L15, TDN, TUP, TSH, _X_, R15, R14, R13, R12, R11, _X_, \
+            _X_, L11, L12, L13, L14, L15, CTI, CTD, CTS, _X_, R15, R14, R13, R12, R11, _X_, \
                            _X_, L01, L02, L03, L04, R04, R03, R02, R01, _X_ )
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = DENSE_LAYOUT(
-              DE_V,     DE_W,     DE_M,     DE_G,       DE_K,              DE_J,     FUNC(SYM_COMM), SYM_DOT,  SYM_QUOT,  DE_B,
-        DE_Q, LG(DE_S), LC(DE_C), LS(DE_N), SYMB(DE_T), TO_MATH,           CW_TOGG,  NAVI(DE_A),     RS(DE_E), RC(DE_I),  RG(DE_H), DE_Z,
-              DE_X,     DE_P,     DE_L,     UMLT(DE_D), TO_NAVI,           TO_SYMB,  NMBR(DE_U),     DE_O,     DE_Y,      DE_F,
-                        KC_DEL,   KC_BSPC,  LA(DE_R),   KC_TAB,            KC_ESC,   LA(KC_SPC),     KC_ENT,   KC_INS
+              DE_V,     DE_W,     DE_M,     DE_G,       TO_NAVI,            CW_TOGG, CS_DOT,     CS_COMM,  CS_QUOT,  DE_B,
+        DE_Q, LG(DE_S), LC(DE_C), LS(DE_N), SYMB(DE_T), DE_K,               DE_J,    NMBR(DE_A), RS(DE_E), RC(DE_I), RG(DE_H), DE_Z,
+              DE_X,     DE_P,     DE_L,     UMLT(DE_D), TO_MATH,            TO_SYMB, FUNC(DE_U), DE_O,     DE_Y,     DE_F,
+                        KC_DEL,   KC_TAB,   NAVI(DE_R), KC_BSPC,            KC_ESC,  LA(KC_SPC), KC_ENT,   KC_INS
     ),
     [_UMLT] = DENSE_LAYOUT(
-                    _______, _______, _______, _______, _______,            _______, _______, _______, _______, _______,
-           _______, DE_SS,   KC_LCTL, KC_LSFT, _______, __XXX__,            CW_TOGG, DE_ADIA, _______, _______, _______, _______,
+                    _______, _______, _______, _______, __XXX__,            _______, _______, _______, _______, _______,
+           _______, DE_SS,   _______, _______, _______, _______,            _______, DE_ADIA, _______, _______, _______, _______,
                     _______, _______, _______, MO_UMLT, __XXX__,            __XXX__, DE_UDIA, DE_ODIA, _______, _______,
-                             _______, _______, KC_LALT, _______,            _______, KC_SPC,  _______, _______
+                             _______, _______, _______, _______,            _______, _______,  _______, _______
     ),
     [_SYMB] = DENSE_LAYOUT(
-                    __XXX__, __XXX__, __XXX__, __XXX__, __XXX__,            SYM_TILD, SYM_EXLM, SYM_DLR,  SYM_EQL, SYM_CIRC,
-           __XXX__, KC_LGUI, KC_LCTL, KC_LSFT, MO_SYMB, __XXX__,            __XXX__, SYM_LPRN, SYM_RPRN, SYM_LCBR, SYM_RCBR, SYM_EURO,
-                    __XXX__, __XXX__, __XXX__, __XXX__, TO_NAVI,             TO_BASE, SYM_SLSH, SYM_ASTR, SYM_PLUS, SYM_PERC,
-                             _______, _______, KC_LALT, _______,            _______,  SYM_MINS, _______,  _______
+                    __XXX__, __XXX__, __XXX__, __XXX__, __XXX__,            _______, CS_EXLM, CS_DLR,  CS_EQL,  CS_CIRC,
+           __XXX__, KC_LGUI, KC_LCTL, KC_LSFT, MO_SYMB, __XXX__,            CS_TILD, CS_LPRN, CS_RPRN, CS_LCBR, CS_RCBR, CS_EURO,
+                    __XXX__, __XXX__, __XXX__, __XXX__, __XXX__,            TO_BASE, CS_SLSH, CS_ASTR, CS_PLUS, CS_PERC,
+                             _______, _______, KC_LALT, _______,            _______, CS_MINS, _______,  _______
     ),
     [_NMBR] = DENSE_LAYOUT(
                        __XXX__, DE_7,    DE_8,    DE_9, __XXX__,            __XXX__, __XXX__, __XXX__, __XXX__, __XXX__,
-              __XXX__, __XXX__, DE_1,    DE_2,    DE_3, TO_MATH,            __XXX__, __XXX__, KC_RSFT, KC_RCTL, KC_RGUI, __XXX__,
-                       __XXX__, DE_4,    DE_5,    DE_6, TO_NAVI,             TO_SYMB,  MO_NMBR,  __XXX__, __XXX__, __XXX__,
+              __XXX__, __XXX__, DE_1,    DE_2,    DE_3, __XXX__,            __XXX__, MO_NMBR, KC_RSFT, KC_RCTL, KC_RGUI, __XXX__,
+                       __XXX__, DE_4,    DE_5,    DE_6, __XXX__,            __XXX__, __XXX__, __XXX__, __XXX__, __XXX__,
                                 _______, _______, DE_0, _______,            _______, KC_LALT, _______, _______
     ),
     [_MATH] = DENSE_LAYOUT(
-                       __XXX__, DE_7,    DE_8,    DE_9, __XXX__,            NUM_TILD, NUM_COMM, NUM_DOT,  NUM_EQL,  NUM_CIRC,
-              __XXX__, __XXX__, DE_1,    DE_2,    DE_3, TO_BASE,            __XXX__,  NUM_LPRN, NUM_RPRN, NUM_LABK, NUM_RABK, NUM_EURO,
-                       __XXX__, DE_4,    DE_5,    DE_6, TO_NAVI,            TO_SYMB,  NUM_SLSH, NUM_ASTR, NUM_PLUS, NUM_PERC,
-                                _______, _______, DE_0, _______,            _______,  NUM_MINS, _______, _______
+                       __XXX__, DE_7,    DE_8,    DE_9, _______,            __XXX__, CM_COMM, CM_DOT,  CM_EQL,  CM_CIRC,
+              __XXX__, __XXX__, DE_1,    DE_2,    DE_3, __XXX__,           CM_TILD,  CM_LPRN, CM_RPRN, CM_LABK, CM_RABK, CM_EURO,
+                       __XXX__, DE_4,    DE_5,    DE_6, TO_BASE,            _______, CM_SLSH, CM_ASTR, CM_PLUS, CM_PERC,
+                                _______, _______, DE_0, _______,            _______, CM_MINS, _______,  _______
     ),
     [_FUNC] = DENSE_LAYOUT(
-                     KC_F10, KC_F7,   KC_F8,   KC_F9,   KC_MUTE,            __XXX__, MO_FUNC,   __XXX__, __XXX__, __XXX__,
-            __XXX__, KC_F11, KC_F1,   KC_F2,   KC_F3,   KC_VOLD,            __XXX__, __XXX__, KC_RSFT, KC_RCTL, KC_RGUI, __XXX__,
-                     KC_F12, KC_F4,   KC_F5,   KC_F6,   KC_VOLU,             TO_SYMB,  __XXX__, __XXX__, __XXX__, __XXX__,
-                             _______, _______, KC_LALT, _______,            _______, KC_LALT, _______, _______
+                     KC_F10, KC_F7,   KC_F8,   KC_F9,   __XXX__,            __XXX__, __XXX__, __XXX__, __XXX__, __XXX__,
+            __XXX__, KC_F11, KC_F1,   KC_F2,   KC_F3,   __XXX__,            __XXX__, __XXX__, KC_RSFT, KC_RCTL, KC_RGUI, __XXX__,
+                     KC_F12, KC_F4,   KC_F5,   KC_F6,   __XXX__,            __XXX__, MO_FUNC, __XXX__, __XXX__, __XXX__,
+                             _______, _______, __XXX__, _______,            _______, KC_LALT, _______, _______
     ),
     [_NAVI] = DENSE_LAYOUT(
-                 KC_PGUP, NAV_COPY, NAV_PSTE, KC_UP,    KC_END,             __XXX__, __XXX__, __XXX__, __XXX__, __XXX__,
-        KC_HOME, KC_LEFT, KC_LCTL,  KC_LSFT,  KC_RIGHT, __XXX__,            __XXX__, MO_NAVI,  KC_RSFT, KC_RCTL, KC_RGUI, __XXX__,
-                 KC_PGDN, NAV_UNDO, NAV_MUTE, KC_DOWN,  __XXX__,            __XXX__, __XXX__, __XXX__, __XXX__, __XXX__,
-                          _______,  _______,  KC_LALT,  _______,            _______, KC_LALT, _______, _______
+                   KC_PGUP, CN_COPY, CN_PSTE, KC_UP,    TO_BASE,            __XXX__, __XXX__, __XXX__, __XXX__, __XXX__,
+          KC_HOME, KC_LEFT, KC_LCTL, KC_LSFT, KC_RIGHT, KC_END,             __XXX__, __XXX__, KC_RSFT, KC_RCTL, KC_RGUI, __XXX__,
+                   KC_PGDN, CN_UNDO, CN_MUTE, KC_DOWN,  _______,            _______, __XXX__, __XXX__, __XXX__, __XXX__,
+                            _______, _______, MO_NAVI,  _______,            _______, KC_LALT, _______, _______
     ),
 };
